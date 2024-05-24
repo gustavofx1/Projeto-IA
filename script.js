21fe69c45923290868e81f046ddca6dc36457e27
gustavofx1 <117363084+gustavofx1@users.noreply.github.com>
2024-05-24 08:46:51 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaALternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".textoresultado");
+
+const perguntas = [
+    enuciado: "Pergunta 1", 
+    alternativas: ["Alternativa 1", "Aternativa 2"]
+ 
+    "Pergunta 2"
+]
